463bed259b40e81f3b53c38daf353038de4a65ec
Pranav Joshi <itspranavjoshi02@gmail.com>
2026-07-05 15:33:44 +0530

    Time: 3 ms (98.31%), Space: 46.6 MB (7.4%) - LeetHub

--- 0437-path-sum-iii/0437-path-sum-iii.java
+++ 0437-path-sum-iii/0437-path-sum-iii.java
@@ -14,21 +14,32 @@
  * }
  */
 class Solution {
-    private int check(TreeNode root, long target){
-        if(root==null){return 0;}
-        int count=0;
-        if(root.val == target){
-            count++;
-        }
-        count += check(root.left , target-root.val);
-        count += check(root.right , target-root.val);
+    HashMap<Long, Integer> map = new HashMap<>();
+
+    public int pathSum(TreeNode root, int targetSum) {
+
+        map.put(0L, 1);
+
+        return dfs(root, 0L, targetSum);
+    }
+
+    private int dfs(TreeNode root, long currSum, int target) {
+
+        if (root == null)
+            return 0;
+
+        currSum += root.val;
+
+        int count = map.getOrDefault(currSum - target, 0);
+
+        map.put(currSum,
+                map.getOrDefault(currSum, 0) + 1);
+
+        count += dfs(root.left, currSum, target);
+        count += dfs(root.right, currSum, target);
+
+        map.put(currSum, map.get(currSum) - 1);
 
         return count;
-    }
-    public int pathSum(TreeNode root, int targetSum) {
-        if(root==null){return 0;}
-        
-       //check every node and its left and right subtree
-        return check(root,targetSum) + pathSum(root.left,targetSum) + pathSum(root.right,targetSum);
     }
 }
