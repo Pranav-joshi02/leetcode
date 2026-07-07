1bbf62a86cb002858a09818996dde47ae132307c
Pranav Joshi <itspranavjoshi02@gmail.com>
2026-07-07 20:25:40 +0530

    Time: 1 ms (99.85%), Space: 42.8 MB (34.98%) - LeetHub

--- 3754-concatenate-non-zero-digits-and-multiply-by-sum-i/3754-concatenate-non-zero-digits-and-multiply-by-sum-i.java
+++ 3754-concatenate-non-zero-digits-and-multiply-by-sum-i/3754-concatenate-non-zero-digits-and-multiply-by-sum-i.java
@@ -1,24 +1,44 @@
 class Solution {
     public long sumAndMultiply(int n) {
-        Deque<Long> deque = new LinkedList<>();
+        // Deque<Long> deque = new LinkedList<>();
 
+        // long sum=0;
+        // while(n>0){
+        //     sum += n%10;
+        //     long t=n%10;
+        //     deque.addLast(t);
+        //     n /= 10;
+        // }
+
+        // long num=0;
+        // while(!deque.isEmpty()){
+        //     long tem=deque.pollLast();
+        //     if(tem!=0){
+        //         num = num*10+ tem;
+        //     }
+        // }
+
+        // return num*sum;
+
+
+         long num=0;
         long sum=0;
+        long ans=0;
         while(n>0){
-            sum += n%10;
-            long t=n%10;
-            deque.addLast(t);
-            n /= 10;
+            if(n%10!=0){
+                num=num*10+n%10;
+                sum+=n%10;
+                
+            }n/=10;
         }
-
-        long num=0;
-        while(!deque.isEmpty()){
-            long tem=deque.pollLast();
-            if(tem!=0){
-                num = num*10+ tem;
-            }
+         while(num!=0){
+            ans=ans*10+num%10;
+            num/=10;
         }
-
-        return num*sum;
-
+        return ans*sum;
     }
+
+
+
+   
 }
