f89e4a76824b868384ad9d3811ed94b6beb8ab16
Pranav Joshi <itspranavjoshi02@gmail.com>
2026-07-08 23:10:27 +0530

    Time: 62 ms (33.37%), Space: 44.6 MB (52.46%) - LeetHub

---
+++ 0279-perfect-squares/0279-perfect-squares.java
@@ -0,0 +1,26 @@
+class Solution {
+    int dp[];
+    private int check(int sum){
+        if(sum==0){return 0;}
+        if(sum<0){return Integer.MAX_VALUE;}
+        if(dp[sum]!=-1){return dp[sum];}
+        int ans= Integer.MAX_VALUE;
+        for(int i=1;i*i<=sum;i++){
+            int result=check(sum-i*i);
+
+            if(result != Integer.MAX_VALUE){
+                ans=Math.min(ans,result+1);
+            }
+        }
+        dp[sum]=ans;
+        return ans;
+    }
+    public int numSquares(int n) {
+        dp = new int[n+1];
+        Arrays.fill(dp,-1);
+
+        return check(n);
+        }
+}
+
+        
