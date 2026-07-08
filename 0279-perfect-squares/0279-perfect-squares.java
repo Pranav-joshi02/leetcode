299a75bddbd719e410ba9ed47dae29110748d80d
Pranav Joshi <itspranavjoshi02@gmail.com>
2026-07-08 23:20:09 +0530

    Time: 65 ms (25%), Space: 44.5 MB (52.46%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.java
+++ 0279-perfect-squares/0279-perfect-squares.java
@@ -4,11 +4,11 @@ class Solution {
         if(sum==0){return 0;}
         if(sum<0){return Integer.MAX_VALUE;}
         if(dp[sum]!=-1){return dp[sum];}
-        int ans= Integer.MAX_VALUE;
+        int ans= 100000; //contraints upto 10^4
         for(int i=1;i*i<=sum;i++){
             int result=check(sum-i*i);
 
-            if(result != Integer.MAX_VALUE){
+            if(result != 100000){
                 ans=Math.min(ans,result+1);
             }
         }
